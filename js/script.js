a234ba31f6d0e431e7789d01605131366b04dd85
PCWitcher <pcwitcher.ie@gmail.com>
2019-07-31 22:35:36 +0100

    add computer moves

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
-let computerMove = `kamień`;
-let playerMove = 'papier';
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+    computerMove = 'kamień'
+} else if (randomNumber == 2) {
+    computerMove = 'papier'
+} else if (randomNumber == 3) {
+    computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);
